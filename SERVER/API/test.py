efbed6826218a578a66d5694c8d98cb47577e85a
Mike Aelbrecht <mikeaelbrecht@gmail.com>
2023-12-26 14:59:42 +0100

    Add NRF24L01 communication and API setup

--- SERVER/API/test.py
+++ SERVER/API/test.py
@@ -2,36 +2,106 @@
 
 import socket
 
+import struct
+import sys
+import time
+import traceback
+
+import pigpio
+from nrf24 import *
+
+
 def handle_data(data) -> None:
     print("Received data:", data)
 
-    if ("/turnOnLight" in data):
+    if "/turnOnLight" in data:
         print("Turning on light...")
-    elif ("/turnOffLight" in data):
+        send_data(1)
+    elif "/turnOffLight" in data:
         print("Turning off light...")
+        send_data(0)
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# server_address = ('framboos20.local', 3000)
-server_address = ('localhost', 3000)
-sock.bind(server_address)
+def send_data(data: int) -> None:
+    try:
+        print(f"Send to {address}")
 
-sock.listen(1)
+        while True:
+            payload = struct.pack("<Bff", 0x01, data)
 
-print("Server is listening on port 3000...")
+            # Send the payload to the address specified above.
+            nrf.reset_packages_lost()
+            nrf.send(payload)
+            try:
+                nrf.wait_until_sent()
+            except TimeoutError:
+                print("Timeout waiting for transmission to complete.")
+                # Wait 10 seconds before sending the next reading.
+                time.sleep(10)
+                continue
 
-while True:
-    # Wait for a connection
-    client_socket, client_address = sock.accept()
+            if nrf.get_packages_lost() == 0:
+                print(
+                    f"Success: lost={nrf.get_packages_lost()}, retries={nrf.get_retries()}"
+                )
+            else:
+                print(
+                    f"Error: lost={nrf.get_packages_lost()}, retries={nrf.get_retries()}"
+                )
 
-    # Receive data from the client
-    data = client_socket.recv(1024).decode("utf-8")
+            # Wait 10 seconds before sending the next reading.
+            time.sleep(10)
+    except:
+        traceback.print_exc()
+        nrf.power_down()
+        pi.stop()
 
-    handle_data(data)
 
-    # Send a response back to the client
-    response = "Hello from the server!"
-    client_socket.sendall(response.encode("utf-8"))
+if __name__ == "__main__":
+    # ---- Setup NRF24L01 ----
+    hostname = "framboos20.local"
+    port = 8888
+    address = "1SNSR"
 
-    # Close the connection
-    client_socket.close()
+    print(f"Connecting to GPIO daemon on {hostname}:{port} ...")
+    pi = pigpio.pi(hostname, port)
+    if not pi.connected:
+        print("Not connected to Raspberry Pi ... goodbye.")
+        sys.exit()
+
+    nrf = NRF24(
+        pi,
+        ce=25,
+        payload_size=RF24_PAYLOAD.DYNAMIC,
+        channel=100,
+        data_rate=RF24_DATA_RATE.RATE_250KBPS,
+        pa_level=RF24_PA.LOW,
+    )
+    nrf.set_address_bytes(len(address))
+    nrf.open_writing_pipe(address)
+
+    # Display the content of NRF24L01 device registers.
+    nrf.show_registers()
+
+    # ---- Setup API ----
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # server_address = ('framboos20.local', 3000)
+    server_address = ("localhost", 3000)
+    sock.bind(server_address)
+
+    sock.listen(1)
+
+    print("Server is listening on port 3000...")
+
+    while True:
+        client_socket, client_address = sock.accept()
+
+        data = client_socket.recv(1024).decode("utf-8")
+
+        handle_data(data)
+
+        response = "Hello from the server!"
+        client_socket.sendall(response.encode("utf-8"))
+
+        client_socket.close()
